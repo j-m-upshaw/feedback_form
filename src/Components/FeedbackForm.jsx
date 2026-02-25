3f19961f6e27f4ae01846011d81d306f236481f9
j-m-upshaw <jalandmupshaw@gmail.com>
2026-02-25 15:38:29 -0600

    Implemented a rating section onto the form.

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -6,7 +6,8 @@ const FeedbackForm = () => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
-    feedback: ''
+    feedback: '',
+    rating: ''
   });
 
   const handleChange = (event) => {
@@ -23,6 +24,7 @@ const FeedbackForm = () => {
       Name: ${formData.name}
       Email: ${formData.email}
       Feedback: ${formData.feedback}
+      Rating: ${formData.rating}
     `;
 
     const isConfirmed = window.confirm(`Please confirm your details:\n\n${confirmationMessage}`);
@@ -31,7 +33,8 @@ const FeedbackForm = () => {
       setFormData({
         name: '',
         email: '',
-        feedback: ''
+        feedback: '',
+        rating: ''
       });
       alert('Thank you for your valuable feedback!');
     }
@@ -66,6 +69,51 @@ const FeedbackForm = () => {
           value={formData.feedback}
           onChange={handleChange}
         ></textarea>
+
+        <div>
+          <span>Rate Us</span>
+          <p>
+            <input
+            type='radio'
+            name='rating'
+            value={1}
+            onChange={handleChange}
+            />1
+          </p>
+          <p>
+            <input
+            type='radio'
+            name='rating'
+            value={2}
+            onChange={handleChange}
+            />2
+          </p>
+          <p>
+            <input
+            type='radio'
+            name='rating'
+            value={3}
+            onChange={handleChange}
+            />3
+          </p>
+          <p>
+            <input
+            type='radio'
+            name='rating'
+            value={4}
+            onChange={handleChange}
+            />4
+          </p>
+          <p>
+            <input
+            type='radio'
+            name='rating'
+            value={5}
+            onChange={handleChange}
+            />5
+          </p>
+        </div>
+
         <button type='submit'>Submit Feedback</button>
       </form>
     </>
